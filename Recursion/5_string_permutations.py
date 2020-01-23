112f0a0f65bd2a9b280f6e8ff415746d536e6a5f
Anonymous <ebrahim994a@gmail.com>
2020-01-22 22:55:21 -0500

    feat: fix

---
+++ Recursion/5_string_permutations.py
@@ -0,0 +1,27 @@
+#permutations
+def permutations(l):
+    if len(l) <= 1:
+        return [l]
+    
+    elif len(l) == 2:
+        return [l, l[::-1]]
+    
+    else:
+        output = []
+        current = l[0]
+        prev = permutations(l[1:])
+
+        for element in prev:
+            for pos in range(len(element) + 1):
+                temp_list = []
+                temp_element = element.copy()
+                for i in range(len(element) + 1):
+                    if pos == i:
+                        temp_list.append(current)
+                    else:
+                        temp_list.append(temp_element.pop())
+                output.append(temp_list)
+        
+        return output
+
+print(permutations([1,2,3,4,5,6]))
